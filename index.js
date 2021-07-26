aa47ab957e5352fcdfcc0ddb5e0fe860acd345ad
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-07-26 10:19:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -282,7 +282,7 @@ import EC from './ec.js'
                             }
                             , 5000);
 
-                            ec.TenguSeederAPI(`${nodeURL}/exit`, 'POST').then(d=>{
+                            ec.TenguSeederAPI(`${nodeURL}/reboot`, 'POST').then(d=>{
                                 console.log(`seeder is being rebooted. d: ${d}`);
                             }
                             ).catch(e=>{
